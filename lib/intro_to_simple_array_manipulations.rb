7f06199b5ba8702a14dfb096d9824ebf250cdc36
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-07-03 19:22:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,9 +16,9 @@ end
 def shift_with_args (array)
   array.shift(2)
 end
-def using_contact (array1, array2)
+def using_concat (array1, array2)
   array1.concat(array2)
 end
 def using_insert (array, element)
-  array[4].insert(element)
+  array.insert[4](element)
 end
